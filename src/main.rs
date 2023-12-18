e70f0f630253aed64473976deb6a360f15453f0b
heko <91366057+hkohko@users.noreply.github.com>
2023-12-19 06:44:36 +0700

    formatting: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-use ureq;
-use select::document::Document;
-use select::predicate::{Name, Attr};
-use select::node::Node;
 use anyhow::Result;
+use select::document::Document;
+use select::node::Node;
+use select::predicate::{Attr, Name};
+use ureq;
 
 fn main() {
     let page = make_request().expect("");
     let _ = scrape(page.as_str());
 }
-fn make_request() -> Result<String>{
+fn make_request() -> Result<String> {
     let url = "https://books.toscrape.com/";
     let agent = ureq::AgentBuilder::new().build();
     let resp = agent.get(url).call()?;
@@ -24,47 +24,42 @@ fn scrape(page: &str) {
             main_div.push(class);
         })
         .collect::<Vec<()>>();
-        
+
     let mut section: Vec<Node<'_>> = Vec::new();
     let _get_section = main_div
         .iter()
         .map(|div| {
-            div
-            .find(Name("section"))
-            .map(|sect| {
-               section.push(sect);
-            })
-            .collect::<Vec<()>>()
+            div.find(Name("section"))
+                .map(|sect| {
+                    section.push(sect);
+                })
+                .collect::<Vec<()>>()
         })
         .collect::<Vec<Vec<()>>>();
-        
+
     let mut h3: Vec<Node<'_>> = Vec::new();
     let _get_h3 = section
-    .iter()
-    .map(|li| {
-        li
-        .find(Name("h3"))
-        .map(|h3s| {
-            h3.push(h3s);
+        .iter()
+        .map(|li| {
+            li.find(Name("h3"))
+                .map(|h3s| {
+                    h3.push(h3s);
+                })
+                .collect::<Vec<()>>()
         })
-        .collect::<Vec<()>>()
-    })
-    .collect::<Vec<Vec<()>>>();
-    
+        .collect::<Vec<Vec<()>>>();
+
     let mut title = Vec::new();
     let _get_a = h3
-    .iter()
-    .map(|h3| {
-        h3
-        .find(Name("a"))
-        .map(|a| {
-            match a.attr("title") {
-                Some(val) => title.push(val),
-                None => (),
-            }
+        .iter()
+        .map(|h3| {
+            h3.find(Name("a"))
+                .map(|a| match a.attr("title") {
+                    Some(val) => title.push(val),
+                    None => (),
+                })
+                .collect::<Vec<()>>()
         })
-        .collect::<Vec<()>>()
-    })
-    .collect::<Vec<Vec<()>>>();
+        .collect::<Vec<Vec<()>>>();
     dbg!(&title);
 }
